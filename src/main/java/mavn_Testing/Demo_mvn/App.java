2fc7e7e67c84ad28df05572784382dd48242e0ed
Sandeep Mandhala <sandeep.mandhala@rabbithyd.com>
2018-12-20 14:24:31 +0530

    Rechange demo mvn

---
+++ src/main/java/mavn_Testing/Demo_mvn/App.java
@@ -0,0 +1,16 @@
+package mavn_Testing.Demo_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	//Hello world
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    //Hello Tester
+    //Hello Tester1
+}
---
+++ src/test/java/mavn_Testing/Demo_mvn/NewTest1.java
@@ -0,0 +1,14 @@
+package mavn_Testing.Demo_mvn;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+
+  @Test
+  public void f() {
+	  
+	  
+  }
+  
+ 
+}
